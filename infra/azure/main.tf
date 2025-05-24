5bbb173dfee8a2423f85470a050121af5120628e
NULLchimp <nullchimp@github.com>
2025-05-24 10:25:18 +0200

    Tried another deployment approach... updated scripts

--- infra/azure/main.tf
+++ infra/azure/main.tf
@@ -22,12 +22,12 @@ variable "environment"     { type = string }
 variable "resource_group"  { type = string }
 variable "node_vm_size" {
   type        = string
-  description = "VM size of the default AKS node-pool"
-  default     = "Standard_B2ms"
+  description = "VM size of the default AKS node-pool - using B1s for minimal cost"
+  default     = "Standard_B1s"
 }
 
 locals {
-  rg_name  = var.resource_group
+  rg_name  = "${var.resource_group}-${var.environment}"
   aks_name = "aks-memgraph-${var.environment}"
 }
 
@@ -40,21 +40,52 @@ resource "azurerm_resource_group" "this" {
 }
 
 ############################
-#  AKS CLUSTER
+#  AKS CLUSTER - MINIMAL COST CONFIGURATION
 ############################
 resource "azurerm_kubernetes_cluster" "this" {
   name                = local.aks_name
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
   dns_prefix          = "memgraph-${var.environment}"
-
+  
+  # Minimal cost settings
+  sku_tier = "Free"
+  
   default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = var.node_vm_size
+    name                = "default"
+    node_count          = 1
+    min_count           = 1
+    max_count           = 2
+    vm_size             = var.node_vm_size
+    enable_auto_scaling = true
+    
+    # Cost optimization
+    os_disk_type = "Ephemeral"
+    os_disk_size_gb = 30
+    
+    # Use spot instances for even lower cost (optional)
+    # priority = "Spot"
+    # eviction_policy = "Delete"
+    # spot_max_price = 0.01
   }
 
-  identity { type = "SystemAssigned" }
+  identity { 
+    type = "SystemAssigned" 
+  }
+  
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "standard"
+  }
+  
+  # Disable unnecessary features for cost
+  private_cluster_enabled = false
+  
+  tags = {
+    Environment = var.environment
+    Purpose     = "memgraph-database"
+    CostCenter  = "minimal"
+  }
 }
 
 ############################
